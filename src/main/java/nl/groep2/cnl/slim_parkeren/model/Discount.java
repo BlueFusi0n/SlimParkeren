0abc24b13beeb894198134a387e64f7c2df59865
Niels de Boer <niels.deboer@live.nl>
2016-10-27 15:54:21 +0200

    Discount changed

--- src/main/java/nl/groep2/cnl/slim_parkeren/model/Discount.java
+++ src/main/java/nl/groep2/cnl/slim_parkeren/model/Discount.java
@@ -10,6 +10,7 @@ public class Discount extends EntityModel {
 	@NotEmpty
 	private String Code;
 	private Date ExpiryDate;
+	@NotEmpty
 	private int Amount; //Amount is number of discountcodes left (-1 = infinite)
 	private int Deal; 	//Deal is number in percentage as discount
 	
